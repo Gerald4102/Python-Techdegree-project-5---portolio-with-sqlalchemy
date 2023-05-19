400a495ebb49c3a1a5a4f49e2899633d5e11a459
Gerald4102 <106319833+Gerald4102@users.noreply.github.com>
2023-05-19 17:45:16 +0100

    added ReadMe and Model class

---
+++ model.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///projects.db"
+db = SQLAlchemy(app)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column('Created', db.DateTime, default=datetime.datetime.now)
+    title = db.Column('Title', db.String)
+    date = db.Column('Date', db.DateTime)
+    description = db.Column('Description', db.String)
+    skills = db.Column('Skills', db.String)
+    link = db.Column('GitHub_link', db.String)
+
+    def __repr__(self):
+        return f'''Project Name:{self.title}
+                Date: {self.date}
+                Description: {self.description}
+                Skills Learnt: {self.skills}
+                GitHub Repo Link: {self.link}'''
