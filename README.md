c0ac5b74d48ffbde3adf0b2461a463b6c4705b65
Gerald4102 <106319833+Gerald4102@users.noreply.github.com>
2023-05-23 14:11:52 +0100

    added app create edit delete layout

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-PORTFOLIO WITH SQLALCHEMY
+# PORTFOLIO WITH SQLALCHEMY
 
 
-Description: 
+## Description: 
 - A portfolio that displays information about my work, which is store in a database.
 - It is a web application using HTML, CSS, Python, Flask and SQLAlchemy.
 - It lets users add, edit or delete project information. 
 
 
-Directions on how to run the project locally:
+### Directions on how to run the project locally:
 - Download the files in the repository.
 - Use "pip install -r requirements.txt" to replicate the virtual environment.
 - Run app.py
 - Open a browser, go to http://127.0.0.0:8000
 
 
-What I learned:
+## What I learned:
 - HTTP (URI, GET & POST requests)
 - HTML Forms (HTML elements, select boxes, radio buttons)
 - Flask with SQLAlchemy (creating a web site or web app with Flask, connecting with an SQLite database using SQLAlchemy, creating HTML templates and connecting static files)
--- app.py
+++ app.py
@@ -29,20 +29,43 @@ def create():
 
 @app.route('/projects/<id>')
 def detail(id):
-    return render_template('detail.html')
+    proj = Project.query.get_or_404(id)
+    date = datetime.datetime.strftime(proj.date, '%B %Y') 
+    skills = proj.skills.split(',')
+    return render_template('detail.html', project=proj, date=date, skills=skills)
 
 
-@app.route('/projects/<id>/edit')
+@app.route('/projects/<id>/edit', methods=['GET', 'POST'])
 def edit(id):
-    pass
-
+    proj = Project.query.get_or_404(id)
+    date_entry = datetime.datetime.strftime(proj.date, '%Y-%m')
+    if request.form:
+        proj.title = request.form['title']
+        date = request.form['date']
+        date = datetime.datetime.strptime(date, '%Y-%m') 
+        proj.date = date
+        proj.description = request.form['desc']
+        proj.skills = request.form['skills']
+        proj.link = request.form['github']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', project=proj, date=date_entry)
 
 @app.route('/projects/<id>/delete')
 def delete(id):
-    pass
+    proj = Project.query.get_or_404(id)
+    db.session.delete(proj)
+    db.session.commit()
+    return redirect(url_for('index'))
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=8000, host='127.0.0.1')
+
+# https://cdn.pixabay.com/photo/2016/11/29/13/14/attractive-1869761_1280.jpg
